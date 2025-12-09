ce1441d57c2753ee03e30efe623253309aad9d4b
daniil <danila546324@gmail.com>
2025-12-09 18:26:49 -0500

    feat: added GET for table entries

--- src/main/java/org/pavlov/glucose_tracker/controller/MainController.java
+++ src/main/java/org/pavlov/glucose_tracker/controller/MainController.java
@@ -8,10 +8,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -38,5 +35,10 @@ public class MainController {
             .toUri();
 
         return ResponseEntity.created(location).body(entry);
+    }
+
+    @GetMapping
+    public List<GlucoseEntry> getAllEntries() {
+        return store;
     }
 }
