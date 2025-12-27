86b8e6f7b6f702acd8d4dfe09c29e0f504cc4941
daniil <danila546324@gmail.com>
2025-12-27 15:57:23 -0500

    refactor: WebConfig.java CORS configuration now exposes "X-Total-Count" (total number of entries) header to be accessed and displayed in front-end. Method may be changed later due to data leak potential

--- src/main/java/org/pavlov/glucose_tracker/config/WebConfig.java
+++ src/main/java/org/pavlov/glucose_tracker/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:5173/")
+            .allowedOrigins("http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
+            .exposedHeaders("X-Total-Count")
             .allowCredentials(true)
             .maxAge(3600);
     }
